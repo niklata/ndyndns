61181522446cf6c85e915ce69a09816ede07236f
njk@darksword.kain.us <njk@darksword.kain.us>
2007-08-16 16:12:43 -0400

    Bump version number.

--- defines.h
+++ defines.h
@@ -1,7 +1,7 @@
 #define MAX_PATH_LENGTH 1024
 #define PID_FILE_DEFAULT "/var/run/ndyndns.pid"
 #define CONF_FILE_DEFAULT "/etc/ndyndns.conf"
-#define NDYNDNS_VERSION "1.03"
+#define NDYNDNS_VERSION "1.04"
 
 #define MAX_BUF 1024
 #define MAXLINE 1024
