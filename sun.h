e3048a19e97cea4317d2f00e48afdaaf71b70ed8
Nicholas J. Kain <nicholas@kain.us>
2008-12-12 02:37:56 -0500

    Add missing sun.h include.

---
+++ sun.h
@@ -0,0 +1,19 @@
+/* sun.h - sun-specific functions include
+ *
+ * (C) 2004-2007 Nicholas J. Kain <njkain at gmail dot com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __NJK_IFCHD_SUN_H_
+#define __NJK_IFCHD_SUN_H_ 1
+char *get_interface_ip(char *ifname);
+#endif
+
